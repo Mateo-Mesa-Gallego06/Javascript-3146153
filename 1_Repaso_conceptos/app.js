1c92067656f3123f34fa06301c5613d38fbaf472
Mateo-Mesa-Gallego06 <mesamateo88@gmail.com>
2025-07-30 15:27:17 -0500

    Aplicacion terminada

--- 1_Repaso_conceptos/app.js
+++ 1_Repaso_conceptos/app.js
@@ -1,19 +1,27 @@
-const imagen = document.querySelector('.moticos');
-const boton1 = document.querySelector('.boton1');
-const boton2 = document.querySelector('.boton2');
-const boton3 = document.querySelector('.boton3');
+//1. Captura de variables
+const pantalla = document.querySelector("#moticos")
+const btn1 = document.querySelector("#boton1")
+const btn2 = document.querySelector("#boton2")
+const btn3 = document.querySelector("#boton3")
 
-
-boton1.addEventListener('click', () => {
-  imagen.style.filter = 'grayscale(100%)';
-});
-
-
-boton2.addEventListener('click', () => {
-  imagen.style.filter = 'blur(5px)';
-});
-
-
-boton3.addEventListener('click', () => {
-  imagen.style.transform = 'scale(1.5) rotate(10deg)';
-});
+//2. Funciones
+function blancoynegro(){
+  console.log("imagen modificada")
+  pantalla.style.filter = "grayscale(100%)"
+/*   btn1.style.background = "green"
+  btn1.style.color = "#2BC17A"
+  btn1.style.heigth = "200px"
+  btn1.style.borderRadius = "50%" */
+}
+function desenfocar(){
+  console.log("imagen modificada")
+  pantalla.style.filter = "blur(5px)"
+}
+function zoom(){
+  console.log("imagen modificada")
+  pantalla.style.transform = "scale(1.5) rotate(30deg)"
+}
+//3. Eventos
+btn1.addEventListener("click", blancoynegro)
+btn2.addEventListener("click", desenfocar)
+btn3.addEventListener("click", zoom)
